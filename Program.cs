488d2b84121db02c2509ad6965abac8c2aa8b706
ch0pppa <000aleks00093@gmail.com>
2024-09-17 23:02:33 +0300

    prog

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson13_hw1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            
+            Console.WriteLine("Your integer is? ");
+            number = int.Parse(Console.ReadLine());
+
+
+            if (number % 2 != 0)
+
+            {
+                Console.WriteLine("Your integer is an odd");
+                return;
+            }
+
+            { 
+                Console.WriteLine("Your integer is an even");
+            }
+        }
+            
+    }
+}
